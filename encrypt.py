11a1852d5193d90459045f5778da53c40507f2f6
Tanvir <info.tanvirx@gmail.com>
2024-11-29 22:08:50 +0800

    encrypt and decrypt initialized

---
+++ encrypt.py
@@ -0,0 +1,29 @@
+alphabet = ["a","b",'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+            'w','x','y','z']
+
+direction = input("TYpe 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("TYpe your message:\n").lower()
+shift = int(input("TYpe the shift number:\n"))
+encrypt_value = ""
+
+
+def encrypt(text,shift,encrypt_value,direction):
+   
+        for character in text:
+            if " " in character:
+                encrypt_value += " "
+            else:
+                real_value = alphabet.index(character)
+                if direction == "encode":
+                    value_find = real_value + shift
+                elif direction == "decode":
+                    value_find = real_value - shift
+                else:
+                    print("put correct encode or decode")
+                    return
+                encrypt_value += alphabet[value_find]
+                
+        print(encrypt_value)         
+
+
+encrypt(text,shift,encrypt_value,direction)
